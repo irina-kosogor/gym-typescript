b78b9eb0328ce0b828fd06e21d76a27315af893f
Irina Kosogor <i.v.kosogor@gmail.com>
2023-10-14 11:46:17 +0300

    add benefits section

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react";
 import { SelectedPage } from "@/shared/types";
 import Navbar from "@/scenes/navbar";
-import Home from "./scenes/home";
+import Home from "@/scenes/home";
+import Benefits from "@/scenes/benefits";
 
 function App() {
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(
@@ -30,6 +31,7 @@ function App() {
         isTopOfPage={isTopOfPage}
       />
       <Home setSelectedPage={setSelectedPage} />
+      <Benefits setSelectedPage={setSelectedPage} />
     </div>
   );
 }
